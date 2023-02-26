9c807b20f485575c3d7996b216d276fcfd881e98
Geetha Kandukuri <113957254+geethakan@users.noreply.github.com>
2023-02-25 19:48:06 -0600

    Update sql_schema.sql

--- static/sql_schema.sql
+++ static/sql_schema.sql
@@ -1,20 +1,20 @@
 create table data (
-   id varchar primary key,
-   category varchar,
-   rank int,
+    id varchar primary key,
+    category varchar,
+    rank int,
     name varchar,
-   location varchar,
+    location varchar,
     imageurl varchar,
     description varchar,
     latitude float(24),
     longitude float(24),
     rate float(24),
-    total_reviews varchar,
-    excellent varchar,
-    very_good varchar,
-    average varchar,
-    poor varchar,
-    terrible varchar
+    total_reviews int,
+    excellent int,
+    very_good int,
+    average int,
+    poor int,
+    terrible int
 );
 
 select * from data
